bb18ccf2802aca4ec60cbd4793158fa8ada952fa
mynguyen8333@gmail.com <mynguyen8333@gmail.com>
2021-09-24 16:07:59 +0700

    xong chuyen dong tu 1 sang 2

--- app/src/main/java/com/example/bai3_1/MainActivity.java
+++ app/src/main/java/com/example/bai3_1/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.bai3_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnGo = findViewById(R.id.btnGo);
+        btnGo.setOnClickListener(view->{
+            startActivity(new Intent(MainActivity.this,GiaoDien3_2.class));
+            overridePendingTransition(R.anim.enter,R.anim.exit);
+        });
+
     }
 }
